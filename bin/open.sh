3533afac8e82dd3dd2864bf2d3f88362b2abc8e8
CHEX <52354962+SFCHEX@users.noreply.github.com>
2022-01-27 08:54:01 +0200

    Add files via upload

--- bin/open.sh
+++ bin/open.sh
@@ -1,2 +1,1 @@
-query=$(:| dmenu -i -p "search:")
-find . 2>/dev/null / | grep -iF "$query" | dmenu -i -l 5 | xargs -0 -I {} xdg-open "{}"
+find . 2>/dev/null / | fzf -m | xargs -0 -I {} xdg-open "{}"
