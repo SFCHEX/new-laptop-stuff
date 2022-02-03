2a33dbc625c05ae475aa40b339ab7d708381d6cc
CHEX <52354962+SFCHEX@users.noreply.github.com>
2022-02-03 09:25:06 +0200

    Add files via upload

--- dwm/dwm-6.2/config.def.h
+++ dwm/dwm-6.2/config.def.h
@@ -42,7 +42,8 @@ static const Rule rules[] = {
         /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
         { "Pavucontrol",  NULL,   NULL,           0,         1,          0,           0         -1 },
         { "Firefox",      NULL,   NULL,           0,         0,          0,          -1,        -1 },
-        { "Brave",      NULL,   NULL,             0,         0,          0,          -1,        -1 },
+        { "Brave",        NULL,   NULL,             0,         0,          0,          -1,        -1 },
+        { "Xdm",          NULL,   NULL,               0,         1,          0,          -1,        -1 },
         { "St",           NULL,   NULL,           0,         0,          1,           0,        -1 },
         { NULL,           NULL,   "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 
--- dwm/dwmblocks/blocks.def.h
+++ dwm/dwmblocks/blocks.def.h
@@ -2,13 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 //	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{" ", "acpi | awk  '{print $3 , $4}' | sed 's/,//g'",	30,		0},
+	{" [", "acpi | awk  '{print $3 , $4}' | sed 's/,//g'",	30,		0},
 
 	{"Volume ", "awk -F\"[][]\" '/Left:/ { print $2 }' <(amixer sget Master)", 	0,		10},
 	{"Brightness ", "brightnessctl -m -d nvidia_0 | awk -F, '{print substr($4, 0,length($4)-1)\"%\"}'", 	0,		20},
-	{"", "date '+%I:%M%p (%a) %b %d '",					60,		0},
+	{"", "date '+%I:%M%p (%a) %b %d] '",					60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "] [";
 static unsigned int delimLen = 5;
