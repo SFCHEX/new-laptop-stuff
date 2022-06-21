bbd54dd9be8fba6d09c3e06add8078380e0afd4b
CHEX <52354962+SFCHEX@users.noreply.github.com>
2022-06-21 14:24:31 +0200

    Add files via upload

--- xmonad/get-brightness.sh
+++ xmonad/get-brightness.sh
@@ -1,1 +1,1 @@
-rightnessctl -m -d nvidia_0 | awk -F, '{print substr($4, 0,length($4)-1)"%"}' & brightnessctl -m -d intel_backlight| awk -F, '{print substr($4, 0,length($4)-1)"%"}'
+brightnessctl -m -d nvidia_0 | awk -F, '{print substr($4, 0,length($4)-1)"%"}' & brightnessctl -m -d intel_backlight| awk -F, '{print substr($4, 0,length($4)-1)"%"}'
